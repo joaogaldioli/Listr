e5dbd9a9385bb0634cfed3fe194a508a530ffe9c
Joao Vivan Galdioli Ferreira de Freitas <34725509+joaogaldioli@users.noreply.github.com>
2023-01-30 20:58:41 -0800

    second commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.listr.app',
+  appName: 'Listr',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
